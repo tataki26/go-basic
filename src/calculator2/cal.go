f427978a539684e63dcc095849d15a59ef315e5c
tataki26 <tataki26@gmail.com>
2022-10-13 11:01:14 +0900

    Create cal.go
    - create calculator program

---
+++ src/calculator2/cal.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("숫자를 입력하세요")
+	// 사용자 입력 객체
+	reader := bufio.NewReader(os.Stdin)
+	// 사용자 입력 읽기(공백까지)
+	line, _ := reader.ReadString('\n')
+	// 불필요한 공백 삭제
+	line = strings.TrimSpace(line)
+
+	// string -> int
+	n1, _ := strconv.Atoi(line)
+
+	line, _ = reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+
+	n2, _ := strconv.Atoi(line)
+
+	fmt.Printf("입력하신 숫자는 %d, %d입니다\n", n1, n2)
+
+	fmt.Println("연산자를 입력하세요")
+
+	line, _ = reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+
+	if line == "+" {
+		fmt.Printf("%d + %d = %d", n1, n2, n1+n2)
+	} else if line == "-" {
+		fmt.Printf("%d - %d = %d", n1, n2, n1-n2)
+	} else if line == "*" {
+		fmt.Printf("%d * %d = %d", n1, n2, n1*n2)
+	} else if line == "/" {
+		fmt.Printf("%d / %d = %d", n1, n2, n1/n2)
+	} else {
+		fmt.Printf("잘못 입력하셨습니다")
+	}
+}
