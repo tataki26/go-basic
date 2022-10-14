874fd9cfe0813b7dd46337a7ee4f694930c3b80a
tataki26 <tataki26@gmail.com>
2022-10-14 10:04:08 +0900

    Update func.go
    - to learn Go recursive function

--- src/function/func.go
+++ src/function/func.go
@@ -22,4 +22,40 @@ func main() {
 
 	a, b := func1(2, 3)
 	fmt.Println(a, b)
+
+	f1(3)
+
+	s := sum(10, 0)
+	fmt.Println(s)
+
+	rtn := fib(10)
+	fmt.Println(rtn)
+}
+
+// recursive function
+func f1(x int) {
+	if x == 0 {
+		return
+	}
+	fmt.Printf("f1(%d) before call f1(%d)\n", x, x-1)
+	f1(x - 1)
+	fmt.Printf("f1(%d) after call f1(%d)\n", x, x-1)
+}
+
+func sum(x, y int) int {
+	if x == 0 {
+		return y
+	}
+	y += x
+	return sum(x-1, y)
+}
+
+func fib(x int) int {
+	if x == 0 {
+		return 1
+	}
+	if x == 1 {
+		return 1
+	}
+	return fib(x-1) + fib(x-2)
 }
