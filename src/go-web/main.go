9ee2f7b202b6341f2de042df74b08869781c652e
tataki26 <tataki26@gmail.com>
2022-10-18 12:24:05 +0900

    Update main.go
    - use Handle method with struct that implements Handler interface

--- src/go-web/main.go
+++ src/go-web/main.go
@@ -5,7 +5,20 @@ import (
 	"net/http"
 )
 
+type fooHandler struct{}
+
+// fooHandler의 메소드
+// Handler 인터페이스의 ServeHTTP 메소드 구현
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Foo!")
+}
+
+func FirstHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "First page")
+}
+
 func main() {
+	// 1
 	// url별로 요청을 처리할 핸들러 함수 등록
 	// "/" 경로(index page)로 접속했을 때 처리할 핸들러 함수 지정
 	// http.ResponseWriter 타입에 값을 쓰면 HTTP 응답으로 전송
@@ -16,10 +29,14 @@ func main() {
 		fmt.Fprintln(w, "Hello, World!")
 	})
 
+	// 1-1
+	// 함수를 따로 정의하고 인자로 넘기기
 	// 경로("/1")에 따른 핸들러
-	http.HandleFunc("/1", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "First page")
-	})
+	http.HandleFunc("/1", FirstHandler)
+
+	// 2
+	// function이 아닌 instance를 등록
+	http.Handle("/foo", &fooHandler{})
 
 	// 웹 서버를 구동하고 웹 요청을 (대기하고) 받아 (미리 지정한 핸들러에 따라) 처리
 	// 웹 요청을 처리할 핸들러를 전달하지 않으면 http.DefaultServeMux가 동작
