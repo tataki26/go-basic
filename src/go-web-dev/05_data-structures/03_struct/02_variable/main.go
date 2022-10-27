94cee3c73d305287aed5bf379fbd83f50d0dc6eb
tataki26 <tataki26@gmail.com>
2022-10-27 17:59:37 +0900

    text template - composite data structures 6
    - parse gohtml file with struct by assigning value

---
+++ src/go-web-dev/05_data-structures/03_struct/02_variable/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type fruit struct {
+	Name  string
+	Color string
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	watermelon := fruit{
+		Name:  "Watermelon",
+		Color: "Green",
+	}
+
+	err := tpl.Execute(os.Stdout, watermelon)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
