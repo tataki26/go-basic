3c4bea817996f3991ba8cb36d651d3cbec678579
tataki26 <tataki26@gmail.com>
2022-10-27 12:03:43 +0900

    text template - composite data structures
    - parse gohtml file with slice

---
+++ src/go-web-dev/05_data-structures/01_slice/01/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	// composite literal - type{data}
+	// string slice
+	fruits := []string{"Apple", "Banana", "Orange", "Mango", "Watermelon"}
+
+	err := tpl.Execute(os.Stdout, fruits)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
