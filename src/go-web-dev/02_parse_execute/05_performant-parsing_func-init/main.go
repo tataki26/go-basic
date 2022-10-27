c5a22a8a43aec17d5f795cc0ca5191ebec1ee62c
tataki26 <tataki26@gmail.com>
2022-10-27 10:47:43 +0900

    text template - parse execute 5
    - check validation of template pointer with Must method

---
+++ src/go-web-dev/02_parse_execute/05_performant-parsing_func-init/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+// template pointer
+var tpl *template.Template
+
+func init() {
+	// Must takes the return values of ParseGlob(pointer, error)
+	// Must checks validation of template pointers from ParseGlob method
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	err := tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "three.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
