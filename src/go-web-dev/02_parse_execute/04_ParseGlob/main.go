5ca958c92fcd26a11598c5796a44a16975661c32
tataki26 <tataki26@gmail.com>
2022-10-27 10:38:56 +0900

    text template - parse execute 4
    - parse multiple gohtml files with ParseGlob
    - argument: file path

---
+++ src/go-web-dev/02_parse_execute/04_ParseGlob/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	// use ParseGlob, no need to list file names
+	tpl, err := template.ParseGlob("templates/*.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "three.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
