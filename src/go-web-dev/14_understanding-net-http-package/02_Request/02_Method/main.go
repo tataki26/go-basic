8234c9f132823088b8eb036a650763ee7f463e28
tataki26 <tataki26@gmail.com>
2022-11-08 16:09:31 +0900

    net/http - Request 2
    - parse "method" and Form using ParseForm method
    - first request: "GET" (form)
    - after button click: "POST" (user input)

---
+++ src/go-web-dev/14_understanding-net-http-package/02_Request/02_Method/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type hotdog int
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// read the request body, parse it as a form and put the result into Request Form
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method      string
+		Submissions url.Values
+	}{
+		req.Method,
+		req.Form,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+
+}
+
+func main() {
+	var h hotdog
+	http.ListenAndServe(":8080", h)
+}
