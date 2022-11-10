258886ea07f62636a5e903345dd3b88ce293e5c1
tataki26 <tataki26@gmail.com>
2022-11-10 14:04:33 +0900

    TCP server - practice 2.4
    - solve the TCP server problem
    - change response header "content-type" to "text/html"
    - change the response to an HTML page

---
+++ src/go-web-dev/19_hands-on/02/08_hands-on/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+			continue
+		}
+
+		serve(conn)
+	}
+
+}
+
+func serve(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			fmt.Println("This is the end of the HTTP Request headers")
+			break
+		}
+	}
+
+	body := `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+			<meta charet="UTF-8">
+			<title></title>
+			</head>
+			<body>
+				<h1>HOLY COW THIS IS LOW LEVEL</h1>
+			</body>
+			</html>
+			`
+
+	io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprintf(conn, "Content-Type: text/html\r\n")
+	io.WriteString(conn, "\r\n")
+	io.WriteString(conn, body)
+
+}
