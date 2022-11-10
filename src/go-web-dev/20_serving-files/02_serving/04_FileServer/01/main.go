8ce63862401c6bce77be4963b051e5e8c30ef26e
tataki26 <tataki26@gmail.com>
2022-11-10 17:25:33 +0900

    File Serving - FileServer
    - use FileServe to serve all files in target directory [open File Server]
    - it returns handler so it can be used as the parameter of Handle method
    - Dir is implemenation of FileSystem interface (open method)

---
+++ src/go-web-dev/20_serving-files/02_serving/04_FileServer/01/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	// FileServer serves file server
+	// Dir(.): current directory(every file in 01 directory)
+	// --> file system / open method
+	// return Handler
+	// give browser "dog.jpg"
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/dog/", dog)
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Contetn-Type", "text/html; charset=utf-8")
+
+	// browser requests "dog.jpg"
+	io.WriteString(w, `<img src="/dog.jpg">`)
+}
