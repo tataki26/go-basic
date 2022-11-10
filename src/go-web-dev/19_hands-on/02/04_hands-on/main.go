e124b363220e04384beec3f517d88fb602001ed6
tataki26 <tataki26@gmail.com>
2022-11-10 10:25:29 +0900

    TCP server - practice 1.4
    - solve the TCP server problem
    - read from the connection
    - change the name of read function into serve
    - add go to enable concurrency and muliple connections

---
+++ src/go-web-dev/19_hands-on/02/04_hands-on/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+			continue
+		}
+
+		go serve(conn)
+	}
+
+}
+
+func serve(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			fmt.Println("This is the end of the HTTP Request headers")
+			break
+		}
+	}
+
+	io.WriteString(conn, "I see you connected.")
+}
