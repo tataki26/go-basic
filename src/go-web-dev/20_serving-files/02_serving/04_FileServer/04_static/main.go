7292db71413f34985bd9649f88b2446289941d35
tataki26 <tataki26@gmail.com>
2022-11-11 13:54:11 +0900

    Error - log.Fatal, http.Error
    - use log.Fatal() to catch the return of ListenAndServe
    - http.Error replies to the request with the specified error message and HTTP code

--- src/go-web-dev/20_serving-files/02_serving/04_FileServer/04_static/main.go
+++ src/go-web-dev/20_serving-files/02_serving/04_FileServer/04_static/main.go
@@ -12,5 +12,15 @@ func main() {
 	// without the final "index.html"
 	// if we have index.html, Dir(.) doesnt' show all contents list in directory --> it shows web page (serve index.html)
 	// redirection: a technique to give more than one URL address to a page/form/web app
+
+	//*** log.Fatal wrapping***
+	// ListenAndServe returns error
+	// wrapping for catching error
+	// log.Fatal: catch any type of error, print it to log file and call os.Exit(1)
+	// 0: success, other: error
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+
+	//*** http.Error() ***
+	// Error replies to the request with the specified error message and HTTP code
+	// ex:) http.Error(w, "file not found", 404)
 }
