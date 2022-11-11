5a1551de04634acd9ab756fa421874e912234621
tataki26 <tataki26@gmail.com>
2022-11-11 16:17:12 +0900

    File Serving - practice 4.1
    - solve the file serving problem
    - serve the files with FileServer and Dir
    - get the images to serve by using StripPrefix
    - remove "public/" so that we can access to "pulic/pic/..." and "public/css/...", not whole content of public directory

---
+++ src/go-web-dev/21_hands-on/05_hands-on/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", dogs)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func dogs(w http.ResponseWriter, req *http.Request) {
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln("template didn't execute: ", err)
+	}
+}
