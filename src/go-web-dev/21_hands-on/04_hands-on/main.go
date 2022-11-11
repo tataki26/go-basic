2057c49ad1246b7a1f162aeada92beb193eb5deb
tataki26 <tataki26@gmail.com>
2022-11-11 15:57:53 +0900

    File Serving - practice 4
    - solve the file serving problem
    - serve the files with FileServer and Dir
    - get the images to serve by using StripPrefix

---
+++ src/go-web-dev/21_hands-on/04_hands-on/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+
+	http.HandleFunc("/", dogs)
+	http.Handle("/resources/", http.StripPrefix("/resources/", fs))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func dogs(w http.ResponseWriter, req *http.Request) {
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln("the template didn't execute:", err)
+	}
+}
