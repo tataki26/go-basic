fd83abdd1f1765b00e633c2ded806b55918eebfb
tataki26 <tataki26@gmail.com>
2022-11-11 15:42:40 +0900

    File Serving - practice 3
    - solve the file serving problem
    - serve the files with FileServer and Dir

---
+++ src/go-web-dev/21_hands-on/03_hands-on/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+
+	http.Handle("/pics/", fs)
+	http.HandleFunc("/", dogs)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func dogs(w http.ResponseWriter, req *http.Request) {
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln("template didn't execute: ", err)
+	}
+}
