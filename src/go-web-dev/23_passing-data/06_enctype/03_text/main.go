fe5471f24cf49436332249dab23c8231835bf5d4
tataki26 <tataki26@gmail.com>
2022-11-14 15:03:23 +0900

    Status - enctype 3
    - text/plain
    - except space, none of words are not encoded
    - only use for debugging (recommended)

---
+++ src/go-web-dev/23_passing-data/06_enctype/03_text/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+
+	// body
+	// create []byte slice with contentLength size
+	bs := make([]byte, req.ContentLength)
+	req.Body.Read(bs)
+	body := string(bs)
+
+	err := tpl.ExecuteTemplate(w, "index.gohtml", body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Fatalln(err)
+	}
+}
