398847171518396cefb2af03ed17d8069b9bb827
tataki26 <tataki26@gmail.com>
2022-11-22 14:34:45 +0900

    Add button eventhandler
    - when we click the button, gameContent is added to viewContent
    - use ReactHtmlParser to show input data without tag

--- src/go-react/simple-react-board/client/src/App.js
+++ src/go-react/simple-react-board/client/src/App.js
@@ -2,12 +2,15 @@ import { useState } from 'react';
 import './App.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser from 'html-react-parser';
 
 function App() {
   const [gameContent, setGameContent] = useState({
     title: '',
     content: ''
   });
+
+  const [viewContent, setViewContent] = useState([]);
 
   const getValue = e => {
     const { name, value } = e.target;
@@ -22,10 +25,14 @@ function App() {
     <div className="App">
       <h1>Movie Review</h1>
       <div className='movie-container'>
-        <h2>제목</h2>
-        <div>
-          내용
-        </div>
+        {viewContent.map(element =>
+          <div>
+            <h2>{element.title}</h2>
+            <div>
+              {ReactHtmlParser(element.content)}
+              </div>
+            </div>
+            )}
       </div>
       <div className='form-wrapper'>
         <input className="title-input" type='text' placeholder='제목' onChange={getValue} name='title'/>
@@ -53,7 +60,11 @@ function App() {
           }}
         />
       </div>
-      <button className="submit-button">입력</button>
+      <button className="submit-button"
+      onClick={() => {
+        setViewContent(viewContent.concat({...gameContent}));
+      }
+      }>입력</button>
     </div>
   );
 }
