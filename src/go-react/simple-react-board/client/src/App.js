fc973bab0bbd1b4df2c4e531ea2552c06c5ce8e6
tataki26 <tataki26@gmail.com>
2022-11-22 14:22:31 +0900

    Add useState code
    - get input data from target and set it to state by using useState
    - it returns array
    - the first element is current state
    - the second element is setter function(update current data)

--- src/go-react/simple-react-board/client/src/App.js
+++ src/go-react/simple-react-board/client/src/App.js
@@ -1,8 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function App() {
+  const [gameContent, setGameContent] = useState({
+    title: '',
+    content: ''
+  });
+
+  const getValue = e => {
+    const { name, value } = e.target;
+      setGameContent({
+        ...gameContent,
+        [name]: value
+      })
+      console.log(gameContent)
+  };
+
   return (
     <div className="App">
       <h1>Movie Review</h1>
@@ -13,7 +28,7 @@ function App() {
         </div>
       </div>
       <div className='form-wrapper'>
-        <input className="title-input" type='text' placeholder='제목' />
+        <input className="title-input" type='text' placeholder='제목' onChange={getValue} name='title'/>
         <CKEditor
           editor={ClassicEditor}
           data="<p>Hello from CKEditor 5!</p>"
@@ -24,6 +39,11 @@ function App() {
           onChange={(event, editor) => {
             const data = editor.getData();
             console.log({ event, editor, data });
+            setGameContent({
+              ...gameContent,
+              content: data
+            })
+            console.log(gameContent);
           }}
           onBlur={(event, editor) => {
             console.log('Blur.', editor);
