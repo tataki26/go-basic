f128fe5b294dc5922043d646945db7652236cc42
tataki26 <tataki26@gmail.com>
2022-11-22 14:05:06 +0900

    Add board UI
    - by updating App.js and index.css code
    - add title, textbox for content and button for submit

--- src/go-react/simple-react-board/client/src/App.js
+++ src/go-react/simple-react-board/client/src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello, World!
+      <h1>Game Review</h1>
+      <div className='game-container'>
+        <h2>제목</h2>
+        <div>
+          내용
+        </div>
+      </div>
+      <div className='form-wrapper'>
+        <input className="title-input" type='text' placeholder='제목' />
+        <textarea className="text-area" placeholder='내용'></textarea>
+      </div>
+      <button className="submit-button">입력</button>
     </div>
   );
 }
