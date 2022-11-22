f01b10cbea73c4d7edfbb8e624f3b78b14f2236b
tataki26 <tataki26@gmail.com>
2022-11-22 14:09:10 +0900

    Add CKEditor5 in form-wrapper
    - CKEditor is WYSIWYG(What You See Is What You Get) editor
    - it makes easy to create the board

--- src/go-react/simple-react-board/client/src/App.js
+++ src/go-react/simple-react-board/client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function App() {
   return (
     <div className="App">
-      <h1>Game Review</h1>
-      <div className='game-container'>
+      <h1>Movie Review</h1>
+      <div className='movie-container'>
         <h2>제목</h2>
         <div>
           내용
@@ -12,7 +14,24 @@ function App() {
       </div>
       <div className='form-wrapper'>
         <input className="title-input" type='text' placeholder='제목' />
-        <textarea className="text-area" placeholder='내용'></textarea>
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
       </div>
       <button className="submit-button">입력</button>
     </div>
