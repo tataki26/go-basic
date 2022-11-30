de65b6c0ce0cc2257d737b76a2498a6938f3eb83
tataki26 <tataki26@gmail.com>
2022-11-30 16:24:19 +0900

    Add IncrementLikes function
    - to use Redis trnsactions, which run multiple commands together as an atomic group
    - use HINCRBY and ZINCRBY together at the same time

--- src/go-redigo/recordstore/albums.go
+++ src/go-redigo/recordstore/albums.go
@@ -48,3 +48,56 @@ func FindAlbum(id string) (*Album, error) {
 
 	return &album, nil
 }
+
+func IncrementLikes(id string) error {
+	conn := pool.Get()
+	defer conn.Close()
+
+	// Before we do anything else, check that an album with the given
+	// id exists. The EXISTS command returns 1 if a specific key exists
+	// in the database, and 0 if it doesn't.
+	exists, err := redis.Int(conn.Do("EXISTS", "album:"+id))
+	if err != nil {
+		return err
+	} else if exists == 0 {
+		return ErrNoAlbum
+	}
+
+	// Use the MULTI command to inform Redis that we are starting a new
+	// transaction. The conn.Send() method writes the command to the
+	// connection's output buffer -- it doesn't actually send it to the
+	// Redis server... despite it's name!
+	err = conn.Send("MULTI")
+	if err != nil {
+		return err
+	}
+
+	// Increment the number of likes in the album hash by 1. Because it
+	// follows a MULTI command, this HINCRBY command is NOT executed but
+	// it is QUEUED as part of the transaction. We still need to check
+	// the reply's Err field at this point in case there was a problem
+	// queueing the command.
+	err = conn.Send("HINCRBY", "album:"+id, "likes", 1)
+	if err != nil {
+		return err
+	}
+
+	// And we do the same with the increment on our sorted set.
+	err = conn.Send("ZINCRBY", "likes", 1, id)
+	if err != nil {
+		return err
+	}
+
+	// Execute both commands in our transaction together as an atomic
+	// group. EXEC returns the replies from both commands but, because
+	// we're not interested in either reply in this example, it
+	// suffices to simply check for any errors. Note that calling the
+	// conn.Do() method flushes the previous commands from the
+	// connection output buffer and sends them to the Redis server.
+	_, err = conn.Do("EXEC")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
