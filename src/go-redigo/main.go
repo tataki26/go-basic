aaeb308a25ecdcc596ca39ca87c625deca09ffb2
tataki26 <tataki26@gmail.com>
2022-11-30 10:22:55 +0900

    Add code to retrieve information from Redis db
    - use HGET command to retrieve information from one of the album hashes
    - with redigo helper functions, we can covert redis reply into Go value

--- src/go-redigo/main.go
+++ src/go-redigo/main.go
@@ -32,4 +32,33 @@ func main() {
 
 	// Success to store data
 	fmt.Println("Electric Ladyland added!")
+
+	// Issue a HGET command to retrieve the title for a specific album,
+	// and use the Str() helper method to convert the reply to a string.
+	title, err := redis.String(conn.Do("HGET", "album:1", "title"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Similarly, get the artist and convert it to a string.
+	artist, err := redis.String(conn.Do("HGET", "album:1", "artist"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// And the price as a float64...
+	price, err := redis.Float64(conn.Do("HGET", "album:1", "price"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// And the number of likes as an integer.
+	likes, err := redis.Int(conn.Do("HGET", "album:1", "likes"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Success to retrieve all information from one of the album hashes
+	fmt.Printf("%s by %s: £%.2f [%d likes]\n", title, artist, price, likes)
+
 }
