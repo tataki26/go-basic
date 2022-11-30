7d41ac5631c563dc596fab6ebfd09a179e42f0eb
tataki26 <tataki26@gmail.com>
2022-11-30 14:50:54 +0900

    Create web server and add ShowAlbum handler
    - initialize the connection pool
    - set up a web server and HTTP handler for the GET /album route

---
+++ src/go-redigo/recordstore/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+func main() {
+	// Initialize a connection pool and assign it to the pool global
+	// variable.
+	pool = &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+	}
+
+	// Create web server
+	mux := http.NewServeMux()
+	mux.HandleFunc("/album", showAlbum)
+	log.Print("Listening on :4000...")
+
+	http.ListenAndServe(":4000", mux)
+}
+
+func showAlbum(w http.ResponseWriter, r *http.Request) {
+	// Unless the request is using the GET method, return a 405 'Method
+	// Not Allowed' response.
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	// Retrieve the id from the request URL query string. If there is
+	// no id key in the query string then Get() will return an empty
+	// string. We check for this, returning a 400 Bad Request response
+	// if it's missing.
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	// Validate that the id is a valid integer by trying to convert it,
+	// returning a 400 Bad Request response if the conversion fails.
+	if _, err := strconv.Atoi(id); err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	// Call the FindAlbum() function passing in the user-provided id.
+	// If there's no matching album found, return a 404 Not Found
+	// response. In the event of any other errors, return a 500
+	// Internal Server Error response.
+	bk, err := FindAlbum(id)
+	if err == ErrNoAlbum {
+		http.NotFound(w, r)
+		return
+	} else if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	// Write the album details as plain text to the client.
+	fmt.Fprintf(w, "%s by %s: £%.2f [%d likes] \n", bk.Title, bk.Artist, bk.Price, bk.Likes)
+}
