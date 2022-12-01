3c1bdee2f6c2d7bb1f5480e53a866f7395e1f3f8
tataki26 <tataki26@gmail.com>
2022-12-01 16:31:34 +0900

    Update UI
    - add title on the top of the page
    - add button color

--- src/go-redigo/04/client/src/App.js
+++ src/go-redigo/04/client/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      안녕하세요.
+      <h1>Connection</h1>
+      <div className='form-wrapper'>
+        <input type="text" name="ethernetid" placeholder="ethernet id" />
+      </div>
+      <button className="submit-button">CONNECT</button>
     </div>
   );
 }
