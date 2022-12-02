5545527368c8d85a514a38f2a6f9100c0012b70a
tataki26 <tataki26@gmail.com>
2022-12-02 09:38:44 +0900

    Add getValue function
    - to get the value (user input) from form

--- src/go-redigo/04/client/src/App.js
+++ src/go-redigo/04/client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 function App() {
+  const getValue = e => {
+    const { name, value } = e.target;
+    console.log(name, value);
+  }
   return (
     <div className="App">
       <h1>Connection</h1>
       <div className='form-wrapper'>
-        <input type="text" name="ethernetid" placeholder="ethernet id" />
+        <input className='id-input' type="text" onChange={getValue} name="ethernetid" placeholder="ethernet id" />
       </div>
       <button className="submit-button">CONNECT</button>
     </div>
