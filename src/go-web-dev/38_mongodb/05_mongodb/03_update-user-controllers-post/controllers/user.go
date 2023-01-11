1c80767dc8f641d9bef31e7f40b3ef6e5fec725d
tataki26 <tataki26@gmail.com>
2023-01-11 15:17:15 +0900

    MongoDB - CRUD 1.2
    - update CreateUser handler
    - get user input and store it in User struct
    - create bson ID and update u.Id
    - store the user in mongodb
    - *** don't run this code *** (objectID)

---
+++ src/go-web-dev/38_mongodb/05_mongodb/03_update-user-controllers-post/controllers/user.go
@@ -0,0 +1,69 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"mongodb/05_mongodb/03_update-user-controllers-post/models"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserController struct {
+	client *mongo.Client
+}
+
+func NewUserController(c *mongo.Client) *UserController {
+	return &UserController{c}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "James Bond",
+		Gender: "male",
+		Age:    32,
+		Id:     p.ByName("id"),
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // 200
+
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+
+	// get user input
+	// store it in User struct
+	json.NewDecoder(r.Body).Decode(&u)
+
+	// create bson ID (update ID)
+	u.Id = primitive.NewObjectID()
+
+	// store the user in mongodb
+	uc.client.Database("go-web-dev-db").Collection("users").InsertOne(context.TODO(), u)
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprint(w, "Write code to delete user\n")
+}
